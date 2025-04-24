df8191ccdfc67f403238722f6dc2934b82531ba3
Windows10V1 <davidvenicu4@gmail.com>
2025-04-24 18:03:34 -0300

    Minor Stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -820,7 +820,7 @@ public class GameManager : MonoBehaviour, IOnEventCallback, IInRoomCallbacks, IC
                 }
             }
             }else{
-                foreach (var hit in Physics2D.OverlapCircleAll(spawnPos, 2)) {
+                foreach (var hit in Physics2D.OverlapCircleAll(spawnPos, 0)) {
                 if (hit.gameObject.CompareTag("Player")) {
                     //cant spawn here
                     remainingSpawns.RemoveAt(index);
